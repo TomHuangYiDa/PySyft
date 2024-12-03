3d5f2b4adf8e72b61748bd48f3202232beeb2864
Koen van der Veen <koenlennartvanderveen@gmail.com>
2024-12-03 15:01:59 +0100

    fix parsing tests

--- syftbox/server/db/schema.py
+++ syftbox/server/db/schema.py
@@ -10,6 +10,7 @@ def get_db(path: str):
         conn.execute("PRAGMA synchronous=OFF;")
         conn.execute("PRAGMA journal_mode=WAL;")
         conn.execute("PRAGMA busy_timeout=5000;")
+        conn.execute("PRAGMA foreign_keys = ON;")
         # Create the table if it doesn't exist
         conn.execute(
             """
@@ -22,52 +23,39 @@ def get_db(path: str):
             last_modified TEXT NOT NULL        )
         """
         )
+        # TODO: migrate file_metadata id?
+
+        # Create a table for storing file information
+        conn.execute(
+            """
+            CREATE TABLE rules (
+                permfile_path varchar(1000) NOT NULL,
+                permfile_dir varchar(1000) NOT NULL,
+                priority INTEGER NOT NULL,
+                path varchar(1000) NOT NULL,
+                user varchar(1000) NOT NULL,
+                can_read bool NOT NULL,
+                can_create bool NOT NULL,
+                can_write bool NOT NULL,
+                admin bool NOT NULL,
+                disallow bool NOT NULL,
+                terminal bool not null,
+                PRIMARY KEY (permfile_path, priority)
+            )
+        """
+        )
+
+        conn.execute(
+            """
+        CREATE TABLE rule_files (
+            permfile_path varchar(1000) NOT NULL,
+            priority INTEGER NOT NULL,
+            file_id INTEGER NOT NULL,
+            match_for_email varchar(1000),
+            PRIMARY KEY (permfile_path, priority, file_id),
+            FOREIGN KEY (permfile_path, priority) REFERENCES rules(permfile_path, priority) ON DELETE CASCADE,
+            FOREIGN KEY (file_id) REFERENCES file_metadata(id) ON DELETE CASCADE
+        );
+        """
+        )
     return conn
-
-
-# TODO: migrate file_metadata id?
-# TODO: think about migrations
-
-connection = sqlite3.connect(":memory:")
-connection.row_factory = sqlite3.Row  # Set row factory to sqlite3.Row
-# connection.row_factory = sqlite3.Row  # Set row factory to sqlite3.Row
-
-
-# Create a cursor object
-cursor = connection.cursor()
-cursor.execute("PRAGMA foreign_keys = ON;")
-
-# Create a table for storing file information
-res = cursor.execute(
-    """
-    CREATE TABLE rules (
-        permfile_path varchar(1000) NOT NULL,
-        permfile_dir varchar(1000) NOT NULL,
-        priority INTEGER NOT NULL,
-        path varchar(1000) NOT NULL,
-        user varchar(1000) NOT NULL,
-        can_read bool NOT NULL,
-        can_create bool NOT NULL,
-        can_write bool NOT NULL,
-        admin bool NOT NULL,
-        disallow bool NOT NULL,
-        terminal bool not null,
-        PRIMARY KEY (permfile_path, priority)
-    )
-"""
-)
-
-
-cursor.execute(
-    """
-CREATE TABLE rule_files (
-    permfile_path varchar(1000) NOT NULL,
-    priority INTEGER NOT NULL,
-    file_id INTEGER NOT NULL,
-    match_for_email varchar(1000),
-    PRIMARY KEY (permfile_path, priority, file_id),
-    FOREIGN KEY (permfile_path, priority) REFERENCES rules(permfile_path, priority) ON DELETE CASCADE,
-    FOREIGN KEY (file_id) REFERENCES file_metadata(id) ON DELETE CASCADE
-);
-"""
-)
--- tests/unit/lib/permission_lib_test.py
+++ tests/unit/lib/permission_lib_test.py
@@ -1,61 +1,72 @@
-
-import yaml
-import pytest
 from pathlib import Path
 
-from syftbox.lib.lib import PermissionRule, PermissionFile, PermissionType, PermissionParsingError
+import pytest
 
+from syftbox.lib.permissions import (
+    PermissionFile,
+    PermissionParsingError,
+    PermissionRule,
+    PermissionType,
+)
+
+
+def test_parsing_dicts():
+    d = {"permissions": "read", "path": "x.txt", "user": "user@example.org"}
+    rule = PermissionRule.from_rule_dict(Path("."), d, priority=0)
+    assert rule.permissions == [PermissionType.READ]
+    assert rule.path == "x.txt"
+    assert rule.user == "user@example.org"
+    assert rule.allow is True
+    assert rule.terminal is False
 
 
 def test_parsing():
     yaml_string = """
     - permissions: read
-    path: x.txt
-    user: user@example.org
-    
+      path: x.txt
+      user: user@example.org
+
     - permissions: [read, write]
-    path: x.txt
-    user: "*"
-    type: disallow
-    terminal: true
+      path: x.txt
+      user: "*"
+      type: disallow
+      terminal: true
     """
-    rule = PermissionRule.from_rule_dict(Path("."), yaml.safe_load(yaml_string)[0], priority=0)
     file = PermissionFile.from_string(yaml_string, ".")
     assert len(file.rules) == 2
 
     assert file.rules[0].permissions == [PermissionType.READ]
     assert file.rules[0].path == "x.txt"
     assert file.rules[0].user == "user@example.org"
-    assert file.rules[0].allow == True
-    assert file.rules[0].terminal == False
+    assert file.rules[0].allow is True
+    assert file.rules[0].terminal is False
 
-
-    #check the same for the second rule
+    # check the same for the second rule
     assert file.rules[1].permissions == [PermissionType.READ, PermissionType.WRITE]
     assert file.rules[1].path == "x.txt"
     assert file.rules[1].user == "*"
-    assert file.rules[1].allow == False
-    assert file.rules[1].terminal == True
+    assert file.rules[1].allow is False
+    assert file.rules[1].terminal is True
 
 
 def test_parsing_fails():
     yaml_string = """
     - permissions: read
-    path: "../*/x.txt"
-    user: user@example.org
+      path: "../*/x.txt"
+      user: user@example.org
     """
     with pytest.raises(PermissionParsingError):
         PermissionFile.from_string(yaml_string, ".")
-        
-        
-    def test_parsing_useremail():
-        yaml_string = """
-    - permissions: read
-    path: "{useremail}/*"
-    user: user@example.org
+
+
+def test_parsing_useremail():
+    yaml_string = """
+        - permissions: read
+          path: "{useremail}/*"
+          user: user@example.org
     """
+
     file = PermissionFile.from_string(yaml_string, ".")
     rule = file.rules[0]
     assert rule.has_email_template
     assert rule.resolve_path_pattern("user@example.org") == "user@example.org/*"
-
