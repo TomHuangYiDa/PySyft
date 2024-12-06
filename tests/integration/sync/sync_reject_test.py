c755e249bff81207bcfec3410ac9eb09885f1546
eelcovdw <eelcovdw@gmail.com>
2024-12-06 14:57:23 +0100

    remove dead code

--- tests/integration/sync/sync_reject_test.py
+++ tests/integration/sync/sync_reject_test.py
@@ -1,47 +1,13 @@
 from pathlib import Path
 
-import faker
-import yaml
 from fastapi.testclient import TestClient
 
 from syftbox.client.base import SyftClientInterface
 from syftbox.client.plugins.sync.manager import SyncManager
 from syftbox.client.plugins.sync.sync_action import format_rejected_path
-from syftbox.client.utils.dir_tree import DirTree, create_dir_tree
+from syftbox.client.utils.dir_tree import create_dir_tree
 from syftbox.lib.constants import PERM_FILE
 from syftbox.lib.permissions import PermissionFile
-from syftbox.server.settings import ServerSettings
-
-fake = faker.Faker()
-
-
-def assert_files_not_on_datasite(client: SyftClientInterface, files: list[Path]):
-    for file in files:
-        assert not (client.workspace.datasites / file).exists(), f"File {file} exists on datasite {client.email}"
-
-
-def assert_files_on_datasite(client: SyftClientInterface, files: list[Path]):
-    for file in files:
-        assert (client.workspace.datasites / file).exists(), f"File {file} does not exist on datasite {client.email}"
-
-
-def assert_files_on_server(server_client: TestClient, files: list[Path]):
-    server_settings: ServerSettings = server_client.app_state["server_settings"]
-    for file in files:
-        assert (server_settings.snapshot_folder / file).exists(), f"File {file} does not exist on server"
-
-
-def assert_dirtree_exists(base_path: Path, tree: DirTree) -> None:
-    for name, content in tree.items():
-        local_path = base_path / name
-
-        if isinstance(content, str):
-            assert local_path.read_text() == content
-        elif isinstance(content, PermissionFile):
-            assert yaml.safe_load(local_path.read_text()) == content.to_dict()
-        elif isinstance(content, dict):
-            assert local_path.is_dir()
-            assert_dirtree_exists(local_path, content)
 
 
 def test_create_without_permission(
