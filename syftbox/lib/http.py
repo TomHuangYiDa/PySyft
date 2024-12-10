b198fdb98cd907f845704c84448e737bebec5254
dk <dkn.work@protonmail.com>
2024-12-10 10:42:35 +0700

    proper fix for `can't encode syftbox.__version__`

--- syftbox/lib/http.py
+++ syftbox/lib/http.py
@@ -1,6 +1,6 @@
 import platform
 
-from syftbox import __version__
+from syftbox.__version__ import __version__
 
 HEADER_SYFTBOX_VERSION = "x-syftbox-version"
 HEADER_SYFTBOX_PYTHON = "x-syftbox-python"
@@ -12,7 +12,7 @@ HEADER_OS_ARCH = "x-os-arch"
 _PYTHON_VERSION = platform.python_version()
 
 SYFTBOX_HEADERS = {
-    HEADER_SYFTBOX_VERSION: str(__version__),
+    HEADER_SYFTBOX_VERSION: __version__,
     HEADER_SYFTBOX_PYTHON: _PYTHON_VERSION,
     # todo - os
 }
