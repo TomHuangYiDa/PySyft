8af9b74b9a9f9fa362d500b94b184bce755a1b2e
Yash Gorana <yash.gorana@hotmail.com>
2024-12-11 23:40:58 +0530

    use grpc

--- syftbox/server/telemetry.py
+++ syftbox/server/telemetry.py
@@ -1,6 +1,6 @@
 from loguru import logger
 from opentelemetry import trace
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import OTELResourceDetector, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
