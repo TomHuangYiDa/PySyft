293d9024b4d038fe1756cc5cb48eaed6516931ca
dk <dkn.work@protonmail.com>
2024-12-12 13:37:24 +0700

    [telemetry] add OS name, version and arch attributes

--- syftbox/lib/http.py
+++ syftbox/lib/http.py
@@ -12,8 +12,19 @@ HEADER_OS_ARCH = b"x-os-arch"
 
 _PYTHON_VERSION = platform.python_version()
 
+# OS info
+uname = platform.uname()
+if uname.system == "Darwin":
+    _OS_NAME = "macos"
+else:
+    _OS_NAME = uname.system.lower()
+_OS_VERSION = uname.release  # e.g. '6.8.0-49-generic'
+_OS_ARCH = uname.machine  # e.g. 'x86_64', 'arm64', 'aarch64', etc.
+
 SYFTBOX_HEADERS = {
     HEADER_SYFTBOX_VERSION: __version__,
     HEADER_SYFTBOX_PYTHON: _PYTHON_VERSION,
-    # todo - os
+    HEADER_OS_NAME: _OS_NAME,
+    HEADER_OS_VERSION: _OS_VERSION,
+    HEADER_OS_ARCH: _OS_ARCH,
 }
