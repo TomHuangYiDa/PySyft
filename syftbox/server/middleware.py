358791f1a32bcc90ab7fa5fa6a964b0d32f93431
dk <dkn.work@protonmail.com>
2024-12-16 15:27:06 +0700

    fix mypy issues for `server/middleware.py`

--- syftbox/server/middleware.py
+++ syftbox/server/middleware.py
@@ -1,12 +1,13 @@
 import time
+from typing import Callable
 
-from fastapi import Request
+from fastapi import Request, Response
 from loguru import logger
 from starlette.middleware.base import BaseHTTPMiddleware
 
 
 class LoguruMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
+    async def dispatch(self, request: Request, call_next: Callable) -> Response:
         start_time = time.time()
         response = await call_next(request)
         duration = time.time() - start_time
