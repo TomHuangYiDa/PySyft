c124f24619c86caf0873bcca68674ed0f14e4f42
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2024-12-19 14:49:09 +0530

    Add server Constraint to limit requests to 10 MB
    Co-authored-by: Shubham Gupta <11032835+shubham3121@users.noreply.github.com>

--- syftbox/server/middleware.py
+++ syftbox/server/middleware.py
@@ -5,6 +5,7 @@ from fastapi import Request, Response
 from loguru import logger
 from starlette.middleware.base import BaseHTTPMiddleware
 
+REQUEST_SIZE_LIMIT_IN_MB = 10
 
 class LoguruMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: Callable) -> Response:
@@ -14,3 +15,14 @@ class LoguruMiddleware(BaseHTTPMiddleware):
         logger.info(f"{request.method} {request.url.path} {response.status_code} {duration:.2f}s")
 
         return response
+
+class RequestSizeLimitMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: Callable) -> Response:
+        request_body = await request.body()
+        if len(request_body) > REQUEST_SIZE_LIMIT_IN_MB * 1024 * 1024:
+            return Response(status_code=413, content=f"Request Denied. Message size is greater than {REQUEST_SIZE_LIMIT_IN_MB} MB")
+
+        response = await call_next(request)
+        return response
+    
+    
