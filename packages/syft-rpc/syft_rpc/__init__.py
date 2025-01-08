809ea988c2384d4363111d7ab92903bc1fe1d46c
Yash Gorana <yash.gorana@hotmail.com>
2025-01-08 16:03:45 +0530

    use uv workspaces

--- packages/syft-rpc/syft_rpc/__init__.py
+++ packages/syft-rpc/syft_rpc/__init__.py
@@ -2,3 +2,5 @@ from .json import JSONModel  # noqa: F401
 from .rpc import Future  # noqa: F401
 from .rpc import Request  # noqa: F401
 from .rpc import SyftBoxURL  # noqa: F401
+
+__version__ = "0.1.0"
