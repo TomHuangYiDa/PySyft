e5fa176fceec7c209dd785207c3f62aae42ae8e5
teo <teo.milea97@gmail.com>
2025-01-14 17:03:01 +0200

    added version checking on the server

--- syftbox/server/middleware.py
+++ syftbox/server/middleware.py
@@ -1,10 +1,14 @@
 import time
 from typing import Callable
 
-from fastapi import Request, Response, status
+from fastapi import HTTPException, Request, Response, status
 from loguru import logger
 from starlette.middleware.base import BaseHTTPMiddleware
+from packaging import version
 
+from syftbox.lib.http import HEADER_GEO_COUNTRY, HEADER_OS_ARCH, HEADER_OS_NAME, HEADER_OS_VERSION, HEADER_SYFTBOX_PYTHON, HEADER_SYFTBOX_USER, HEADER_SYFTBOX_VERSION
+
+MIN_SUPPORTED_VERSION = "0.3.0"
 
 class LoguruMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: Callable) -> Response:
@@ -41,5 +45,24 @@ class RequestSizeLimitMiddleware(BaseHTTPMiddleware):
                 content=f"Request Denied. Message size is greater than {request_size_limit_in_mb} MB",
             )
 
+        response = await call_next(request)
+        return response
+    
+class VersionCheckMIddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: Callable) -> Response: 
+        logger.info(request.headers)
+        client_version = request.headers.get(HEADER_SYFTBOX_VERSION.decode('utf-8'))
+        if not client_version:
+            return Response(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content="Client version not provided. Please include the 'Version' header."
+            )
+        
+        if version.parse(client_version) < version.parse(MIN_SUPPORTED_VERSION):
+            return Response(
+                status_code=status.HTTP_426_UPGRADE_REQUIRED,
+                content=f"Client version is too old. Minimum version required is {MIN_SUPPORTED_VERSION}",
+            )
+        
         response = await call_next(request)
         return response
