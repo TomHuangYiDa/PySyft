0fcc165d89a85ad0704a5318ea134e4de3ea0c44
Shubham Gupta <11032835+shubham3121@users.noreply.github.com>
2025-01-22 20:35:19 +0530

    pass file path during request dump

--- packages/syft-rpc/syft_rpc/rpc.py
+++ packages/syft-rpc/syft_rpc/rpc.py
@@ -63,8 +63,7 @@ def send(
     local_path = syft_request.url.to_local_path(client.workspace.datasites)
     file_path = local_path / f"{syft_request.ulid}.request"
     local_path.mkdir(parents=True, exist_ok=True)
-    output = syft_request.dump()
-    file_path.write_text(output)
+    syft_request.dump(file_path)
 
     future = SyftFuture(ulid=syft_request.ulid, url=syft_request.url)
     return future
