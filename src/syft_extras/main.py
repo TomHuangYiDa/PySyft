b456ea5b1f73e58320a4316d5a14aa3ef23c5c03
dk <dkn.work@protonmail.com>
2025-01-24 09:11:28 +0700

    removing syft-proxy as a workspace

--- src/syft_extras/main.py
+++ src/syft_extras/main.py
@@ -9,7 +9,6 @@ import syft_files
 def main():
     print("syft-core", syft_core.__version__)
     print("syft-event", syft_event.__version__)
-    print("syft-proxy", syft_proxy.__version__)
     print("syft-requests", syft_rpc.__version__)
     print("syft-rpc", syft_requests.__version__)
     print("syft-files", syft_files.__version__)
